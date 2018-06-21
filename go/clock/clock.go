ab87e29161fb7039c705e217b57f001223e74ac8
Alex Ciminian <alex.ciminian@gmail.com>
2018-06-21 22:30:11 +0100

    Refactor clock solution: simplify internal functions

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -6,17 +6,24 @@ type Clock struct {
 	hour, minute int
 }
 
+func cap(num, max int) int {
+	return (num%max + max) % max
+}
+
+func hours(minute int) int {
+	carry := minute / 60
+	if minute < 0 && minute%60 != 0 {
+		carry -= 1
+	}
+	return carry
+}
 func New(hour, minute int) Clock {
-	return Clock{hour: 0, minute: 0}.Add(hour*60 + minute)
+	hour += hours(minute)
+	return Clock{hour: cap(hour, 24), minute: cap(minute, 60)}
 }
 
 func (c Clock) Add(minutes int) Clock {
-	t := minutes + c.minute
-	carry := t / 60
-	if t < 0 && t%60 != 0 {
-		carry -= 1
-	}
-	return Clock{hour: (c.hour + carry%24 + 24) % 24, minute: (t%60 + 60) % 60}
+	return New(c.hour, c.minute+minutes)
 }
 
 func (c Clock) Subtract(minutes int) Clock {
